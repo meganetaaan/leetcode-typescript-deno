ccc44f51595db25e3dd1592bed251d93224e486f
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2020-08-04 15:02:23 +0000

    Fix too long testname

--- lib/test.ts
+++ lib/test.ts
@@ -1,5 +1,21 @@
-export type TestData<T extends (...args: any) => any> = {
+export type TestData<T extends (...args: any[]) => any> = {
   args: Parameters<T>,
   argsAfter?: Parameters<T>,
+  name?: string,
   expected: ReturnType<T>,
 };
+
+import {
+  assertEquals,
+} from "https://deno.land/std@0.60.0/testing/asserts.ts";
+
+export function test<T extends (...args: any[]) => any>(f: T, data: TestData<T>[]) {
+  data.forEach((t, i) => {
+    Deno.test({
+      name: `${String(i)}: "${t.name ?? t.args}"`,
+      fn(): void {
+        assertEquals(f(...t.args), t.expected);
+      },
+    });
+  });
+}
